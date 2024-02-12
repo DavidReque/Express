6df40e96648834767b1ecf6d5fed4c38370a43a9
David Requeno <davidrequeno52@gmail.com>
2024-02-11 20:02:08 -0600

    Recuperar una película por id

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const movies = require('./movies.json')
 const app = express()
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hola' })
-})
-
 app.get('/movies', (req, res) => {
   res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+
+  if (movie) {
+    return res.json(movie)
+  }
+
+  res.statusCode(404).json({ message: 'Not found' })
 })
 
 const PORT = process.env.PORT ?? 1234
