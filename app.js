7c0320c62a3463f96faabae5ac9bc2c5b2ab3100
David Requeno <davidrequeno52@gmail.com>
2024-02-16 20:33:21 -0600

    patch

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const movies = require('./movies.json')
 const crypto = require('node:crypto')
-const { validateMovie } = require('./schema/movies')
+const { validateMovie, validatePartialMovie } = require('./schema/movies')
 
 const app = express()
 app.use(express.json())
@@ -44,6 +44,30 @@ app.get('/movies/:id', (req, res) => {
   }
 
   res.statusCode(404).json({ message: 'Not found' })
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
 })
 
 const PORT = process.env.PORT ?? 1234
