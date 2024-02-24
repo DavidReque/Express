833f49156548dd6c7e0b2ab9ff279fa10041cbb0
David Requeno <davidrequeno52@gmail.com>
2024-02-23 21:30:40 -0600

    controllers

--- routes/movies.js
+++ routes/movies.js
@@ -1,59 +1,14 @@
 import { Router } from 'express'
-import { validateMovie, validatePartialMovie } from '../schema/movies.js'
-import { MovieModel } from '../models/movie.js'
+import { MovieController } from '../controllers/movie.js'
 
 export const moviesRouter = Router()
 
-moviesRouter.get('/', async (req, res) => {
-  const { genre } = req.query
-  const movies = await MovieModel.getAll({ genre })
-  res.json(movies)
-})
+moviesRouter.get('/', MovieController.getAll)
 
-moviesRouter.get('/:id', async (req, res) => {
-  const { id } = req.params
-  const movie = await MovieModel.getById({ id })
+moviesRouter.get('/:id', MovieController.getById)
 
-  if (movie) {
-    return res.json(movie)
-  }
+moviesRouter.post('/', MovieController.create)
 
-  res.statusCode(404).json({ message: 'Not found' })
-})
+moviesRouter.delete('/:id', MovieController.delete)
 
-moviesRouter.post('/', async (req, res) => {
-  const result = validateMovie(req.body)
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const newMovie = await MovieModel.create({ input: result.data })
-
-  res.status(201).json(newMovie)
-})
-
-moviesRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-  const result = await MovieModel.delete({ id })
-
-  if (result === false) {
-    return res.status(404)
-  }
-
-  return res.json({ message: 'movie deleted' })
-})
-
-moviesRouter.patch('/:id', async (req, res) => {
-  const result = validatePartialMovie(req.body)
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { id } = req.params
-
-  const updateMovie = await MovieModel.update({ id, input: result.data })
-
-  return res.json(updateMovie)
-})
+moviesRouter.patch('/:id', MovieController.update)
