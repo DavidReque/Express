424b988e17b46b7e54ecc4b34a13cd2e134d726f
David Requeno <davidrequeno52@gmail.com>
2024-02-27 15:05:08 -0600

    result

--- models/mysql/movie.js
+++ models/mysql/movie.js
@@ -1,5 +1,22 @@
+import mysql from 'mysql2/promise'
+
+const config = {
+  host: 'localhost',
+  user: 'root',
+  port: 3306,
+  password: '9946',
+  database: 'moviesdb'
+}
+
+const connection = await mysql.createConnection(config)
+
 export class MovieModel {
   static async getAll ({ genre }) {
+    const result = await connection.query(
+      'SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) FROM movie;      '
+    )
+
+    console.log(result)
   }
 
   static async getById ({ id }) {
