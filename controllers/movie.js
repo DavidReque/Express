aceed279fb8194d2e9c21d26518525f619528d5f
David Requeno <davidrequeno52@gmail.com>
2024-03-02 12:58:38 -0600

    servers

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,17 +1,20 @@
 // import { MovieModel } from '../models/local-file-system/movie.js'
-import { MovieModel } from '../models/mysql/movie.js'
 import { validateMovie, validatePartialMovie } from '../schema/movies.js'
 
 export class MovieController {
-  static async getAll (req, res) {
+  constructor ({ movieModel }) {
+    this.movieModel = movieModel
+  }
+
+  getAll = async (req, res) => {
     const { genre } = req.query
-    const movies = await MovieModel.getAll({ genre })
+    const movies = await this.movieModel.getAll({ genre })
     res.json(movies)
   }
 
-  static async getById (req, res) {
+  getById = async (req, res) => {
     const { id } = req.params
-    const movie = await MovieModel.getById({ id })
+    const movie = await this.movieModel.getById({ id })
 
     if (movie) {
       return res.json(movie)
@@ -20,21 +23,21 @@ export class MovieController {
     res.statusCode(404).json({ message: 'Not found' })
   }
 
-  static async create (req, res) {
+  create = async (req, res) => {
     const result = validateMovie(req.body)
 
     if (!result.success) {
       return res.status(400).json({ error: JSON.parse(result.error.message) })
     }
 
-    const newMovie = await MovieModel.create({ input: result.data })
+    const newMovie = await this.movieModel.create({ input: result.data })
 
     res.status(201).json(newMovie)
   }
 
-  static async delete (req, res) {
+  delete = async (req, res) => {
     const { id } = req.params
-    const result = await MovieModel.delete({ id })
+    const result = await this.movieModel.delete({ id })
 
     if (result === false) {
       return res.status(404)
@@ -43,7 +46,7 @@ export class MovieController {
     return res.json({ message: 'movie deleted' })
   }
 
-  static async update (req, res) {
+  update = async (req, res) => {
     const result = validatePartialMovie(req.body)
 
     if (!result.success) {
@@ -52,7 +55,7 @@ export class MovieController {
 
     const { id } = req.params
 
-    const updateMovie = await MovieModel.update({ id, input: result.data })
+    const updateMovie = await this.movieModel.update({ id, input: result.data })
 
     return res.json(updateMovie)
   }
